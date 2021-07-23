99c4ee1ee8e86053dc3974da6db4e2cb07fe7103
Henrique <henrique.garcia@leanonsystems.com>
2021-07-23 17:05:47 -0300

    first commit

--- src/templates/list.js
+++ src/templates/list.js
@@ -1,28 +1,32 @@
-import React from "react"
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-import {Link} from "gatsby";
+import React from 'react'
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+import { Link } from 'gatsby'
 
 const ListPage = (args) => {
-    const { pageContext } = args
-    return (
-      <Layout>
-        <SEO title={pageContext.title || `List`} />
-        <h1>{pageContext.title}</h1>
-          {
-              pageContext.nodes.map(node => (
-                <div key={node.id}>
-                    {pageContext.detailsPageUrl &&
-                        <Link to={`${pageContext.detailsPageUrl}/${node[pageContext.detailsPageKey]}`}>
-                            {node.displayName}
-                        </Link>
-                    }
-                    {!pageContext.detailsPageUrl && <span>{node.displayName}</span>}
-                </div>
-          ))
-          }<br/>
-      </Layout>
-    )
+  const { pageContext } = args
+  console.log(pageContext)
+  return (
+    <Layout>
+      <SEO title={pageContext.title || `List`} />
+      <h1>{pageContext.title}</h1>
+      {pageContext.nodes.map((node) => (
+        <div key={node.id}>
+          {pageContext.detailsPageUrl && (
+            <Link
+              to={`${pageContext.detailsPageUrl}/${
+                node[pageContext.detailsPageKey]
+              }`}
+            >
+              {node.displayName}
+            </Link>
+          )}
+          {!pageContext.detailsPageUrl && <span>{node.displayName}</span>}
+        </div>
+      ))}
+      <br />
+    </Layout>
+  )
 }
 
 export default ListPage
